0dcb34b17a5c4590d5c05296f1434924273dd06b
Chris Sevilleja <chris@scotch.io>
2014-04-22 07:58:16 -0700

    Update magic.js

--- magic.js
+++ magic.js
@@ -20,7 +20,8 @@ $(document).ready(function() {
 			type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
 			url 		: 'process.php', // the url where we want to POST
 			data 		: formData, // our data object
-			dataType 	: 'json' // what type of data do we expect back from the server
+			dataType 	: 'json', // what type of data do we expect back from the server
+			encode 		: true
 		})
 			// using the done promise callback
 			.done(function(data) {
